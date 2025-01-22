ce55376134fffb6e7df28ef5e6a5728ca5b73d43
이태관 <cokid98@naver.com>
2025-01-22 23:32:47 +0900

    Feat: px을 rem으로 변환시켜주는 함수 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 import pxToRem from "tailwindcss-preset-px-to-rem";
 
+const pxToRemFunc = (px) => {
+  return `${px / 16}rem`;
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,jsx,js}"],
   presets: [pxToRem],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "layout-padding-x": pxToRemFunc(64),
+      },
+    },
   },
   plugins: [],
 };
