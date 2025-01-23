37782340fa6ab4322c8e297a680378a5f9294cd7
이태관 <cokid98@naver.com>
2025-01-23 17:16:06 +0900

    Feat: svgr플러그인 추가
    - svgr플러그인을 추가해서 svg파일을 컴포넌트처럼 사용하도록 변경

--- src/components/TypeBadge/TypeBadge.jsx
+++ src/components/TypeBadge/TypeBadge.jsx
@@ -1,7 +1,13 @@
 import typeToKorean from "@/utils/typeToKorean";
+import Poison from "@/asset/icon/poison.svg";
 
 function TypeBadge({ type }) {
-  return <div>{typeToKorean(type)}</div>;
+  return (
+    <div>
+      {typeToKorean(type)}
+      <Poison />
+    </div>
+  );
 }
 
 export default TypeBadge;
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import svgr from "vite-plugin-svgr";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
