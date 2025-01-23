f732fcbe8f68f579a8c6a1596fb29bdb27115a41
이태관 <cokid98@naver.com>
2025-01-23 10:48:54 +0900

    Chore: 프로젝트 breakpoint 커스터마이징

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
   content: ["./src/**/*.{html,jsx,js}"],
   presets: [pxToRem],
   theme: {
+    screens: {
+      mobile: { min: "0px", max: "480px" },
+      tablet: { min: "481px", max: "1023px" },
+      desktop: { min: "1024px" },
+    },
     extend: {
       spacing: {
         "layout-padding-x": pxToRemFunc(64),
