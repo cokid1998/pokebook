b959450f92e2f51c880a8c56e5ef43b7bf9d9684
이태관 <cokid98@naver.com>
2025-01-28 00:23:32 +0900

    Feat: 모달을 열었을 때 Loading 처리

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,4 +1,29 @@
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ type }) => {
+  // tailwind는 스타일로 컴파일되는 시점에서 코드를 지워버린다.
+  // 따라서 아래처럼 빌드시 완전한 클래스 네임을 만들어서 bracket notation으로 접근해야한다
+  // https://tailwindcss.com/docs/detecting-classes-in-source-files
+  const typeColor = {
+    normal: "[#949495]",
+    water: "[#5185C5]",
+    fighting: "[#E09C40]",
+    grass: "[#66A945]",
+    flying: "[#A2C3E7]",
+    electric: "[#FBB917]",
+    poison: "[#735198]",
+    psychic: "[#DD6B7B]",
+    ground: "[#9C7743]",
+    ice: "[#6DC8EB]",
+    rock: "[#BFB889]",
+    dragon: "[#535CA8]",
+    bug: "[#9FA244]",
+    dark: "[#4C4948]",
+    ghost: "[#684870]",
+    fairy: "[#DAB4D4]",
+    steel: "[#69A9C7]",
+    fire: "[#E56C3E]",
+    stellar: "[#44628D]",
+  };
+
   return (
     <div className="flex flex-col items-center gap-4">
       <div
@@ -6,7 +31,9 @@ const LoadingSpinner = () => {
         aria-label="로딩 중"
         className="relative w-64 h-64 animate-spin"
       >
-        <div className="absolute w-full h-1/2 rounded-t-full bg-gradient-to-b from-red-600 to-red-500 shadow-lg">
+        <div
+          className={`absolute w-full h-1/2 rounded-t-full shadow-lg bg-${typeColor[type]}`}
+        >
           <div className="absolute bottom-0 left-0 right-0 h-[4px] bg-gray-800" />
           <div className="absolute top-[15%] left-[10%] w-[35%] h-[20%] bg-white/20 rounded-full transform -rotate-12" />
         </div>
@@ -17,7 +44,9 @@ const LoadingSpinner = () => {
           <div className="absolute top-[15%] left-[15%] w-[30%] h-[30%] bg-white rounded-full" />
         </div>
       </div>
-      <div className="text-gray-700 font-medium animate-pulse">Loading...</div>
+      <div className={`text-${typeColor[type]} font-black animate-pulse`}>
+        Loading...
+      </div>
     </div>
   );
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,6 +5,7 @@ import { X } from "lucide-react";
 import TypeBadge from "@/components/TypeBadge/TypeBadge";
 import getModalPokemonData from "@/api/getModalPokemonData";
 import ShinyToggle from "@/components/Modal/ShinyToggle";
+import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
 
 function Modal({ modalRef, isModalOpen, setIsModalOpen, pokemon }) {
   const [showShiny, setShowShiny] = useState(false);
@@ -19,9 +20,9 @@ function Modal({ modalRef, isModalOpen, setIsModalOpen, pokemon }) {
     // gcTime: 10000,
   });
 
-  if (isLoading) return null;
+  const { flavorTextKo, genusKo, abilitiesInfoKo, shinyGif } = data || {};
 
-  const { flavorTextKo, genusKo, abilitiesInfoKo, shinyGif } = data;
+  console.log(types[0]);
 
   return (
     <div
@@ -31,73 +32,77 @@ function Modal({ modalRef, isModalOpen, setIsModalOpen, pokemon }) {
       onClick={() => setIsModalOpen((prev) => !prev)}
       ref={modalRef}
     >
-      <div
-        className={`bg-white dark:bg-gray-800 rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto  ${
-          isModalOpen ? "animate-fade-in" : "animate-fade-out"
-        }`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="p-24">
-          <div className="flex justify-between items-start">
-            <div>
-              <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-                {name}
-              </h2>
-            </div>
-            <button
-              onClick={() => setIsModalOpen((prev) => !prev)}
-              className="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300"
-            >
-              <X size={24} />
-            </button>
-          </div>
-          <div className="mt-24 flex justify-center">
-            <div className="relative text-center">
-              <div className="mobile:w-192 mobile:h-192 w-252 h-252 flex items-center justify-center bg-gray-50 dark:bg-gray-700 rounded-lg">
-                <img
-                  src={showShiny ? shinyGif : gif}
-                  alt={pokemon.name}
-                  className="mobile:w-128 mobile:h-128 w-188 h-188 object-contain pixelated"
-                  style={{
-                    imageRendering: "pixelated",
-                  }}
-                />
+      {isLoading ? (
+        <LoadingSpinner type={types[0]} />
+      ) : (
+        <div
+          className={`bg-white dark:bg-gray-800 rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto  ${
+            isModalOpen ? "animate-fade-in" : "animate-fade-out"
+          }`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="p-24">
+            <div className="flex justify-between items-start">
+              <div>
+                <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+                  {name}
+                </h2>
               </div>
-              <ShinyToggle setShowShiny={setShowShiny} />
+              <button
+                onClick={() => setIsModalOpen((prev) => !prev)}
+                className="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300"
+              >
+                <X size={24} />
+              </button>
             </div>
-          </div>
-          <div className="mt-24">
-            <p className="text-gray-700 dark:text-gray-300 font-black text-24">
-              {genusKo}
-            </p>
-            <div className="mt-16 flex gap-8">
-              {types.map((type, index) => (
-                <TypeBadge key={index} type={type} />
-              ))}
-            </div>
-          </div>
-          <p className="mt-24 text-gray-700 dark:text-gray-300">
-            {flavorTextKo}
-          </p>
-          <div className="mt-24">
-            <h3 className="font-semibold mb-8 text-gray-900 dark:text-white">
-              {name}의 특성
-            </h3>
-            {abilitiesInfoKo.map((ability, index) => {
-              return (
-                <div key={index} className="mb-8">
-                  <span className="font-medium text-gray-900 dark:text-white">
-                    {ability.abilityNameKo}:{" "}
-                  </span>
-                  <span className="text-gray-700 dark:text-gray-300">
-                    {ability.abilityFlavorTextKo}
-                  </span>
+            <div className="mt-24 flex justify-center">
+              <div className="relative text-center">
+                <div className="mobile:w-192 mobile:h-192 w-252 h-252 flex items-center justify-center bg-gray-50 dark:bg-gray-700 rounded-lg">
+                  <img
+                    src={showShiny ? shinyGif : gif}
+                    alt={pokemon.name}
+                    className="mobile:w-128 mobile:h-128 w-188 h-188 object-contain pixelated"
+                    style={{
+                      imageRendering: "pixelated",
+                    }}
+                  />
                 </div>
-              );
-            })}
+                <ShinyToggle setShowShiny={setShowShiny} />
+              </div>
+            </div>
+            <div className="mt-24">
+              <p className="text-gray-700 dark:text-gray-300 font-black text-24">
+                {genusKo}
+              </p>
+              <div className="mt-16 flex gap-8">
+                {types.map((type, index) => (
+                  <TypeBadge key={index} type={type} />
+                ))}
+              </div>
+            </div>
+            <p className="mt-24 text-gray-700 dark:text-gray-300">
+              {flavorTextKo}
+            </p>
+            <div className="mt-24">
+              <h3 className="font-semibold mb-8 text-gray-900 dark:text-white">
+                {name}의 특성
+              </h3>
+              {abilitiesInfoKo.map((ability, index) => {
+                return (
+                  <div key={index} className="mb-8">
+                    <span className="font-medium text-gray-900 dark:text-white">
+                      {ability.abilityNameKo}:{" "}
+                    </span>
+                    <span className="text-gray-700 dark:text-gray-300">
+                      {ability.abilityFlavorTextKo}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
