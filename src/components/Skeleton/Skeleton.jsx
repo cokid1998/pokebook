2b843effceca472b51024ced51cf67bb8d284c84
이태관 <cokid98@naver.com>
2025-01-28 00:44:13 +0900

    Style: Skeleton 부모 태그 px 제거

--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -3,7 +3,7 @@ const tmp = ["", "", "", "", "", "", "", "", ""];
 function Skeleton({ style }) {
   return (
     <main
-      className={`flex-1 w-full bg-gray-50 dark:bg-gray-900 px-16 py-32 h-content-height ${style}`}
+      className={`flex-1 w-full bg-gray-50 dark:bg-gray-900 py-32 h-content-height ${style}`}
     >
       <div className="max-w-7xl mx-auto">
         <div className="grid mobile:grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-24 mb-24">
