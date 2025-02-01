1520901be152600e038c77ba692537ecddd2f3d4
이태관 <cokid98@naver.com>
2025-02-01 12:44:46 +0900

    Chore: 설정파일을 TS로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
-import svgr from "vite-plugin-svgr";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
